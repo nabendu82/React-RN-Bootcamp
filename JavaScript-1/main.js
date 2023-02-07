53fa5685f4e94e44f776cc3f0060c71be5c32c8c
nabendu82 <nabendu.biswas@gmail.com>
2023-02-07 19:46:31 +0530

    first commit

---
+++ JavaScript-1/main.js
@@ -0,0 +1,44 @@
+// console.log("Welcome to JavaScript");
+// var firstName = 'Nabendu';
+// var age = 40;
+// var isDeveloper = true;
+// console.log(firstName);
+// console.log(age);
+// console.log(isDeveloper);
+
+// function varExp(){
+//     if(true){
+//         var num1 = 10;
+//         console.log("Inside if var " + num1);
+//     }
+//     console.log("Outside if var " + num1);
+// }
+
+// varExp();
+// console.log("Outside function var " + num1);
+
+
+// if(true){
+//     let num2 = 20;
+//     console.log("Inside if let " + num2);
+// }
+// console.log("Outside if let " + num2);
+
+// const num3 = 40;
+// num3 = 55;
+
+//Primitive data Types - String, Number, boolean, null, undefined, Symbols
+const name = 'Nabendu';
+const age = 40;
+const rating = 5.5;
+const isDeveloper = true;
+const x = null;
+let y;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof rating);
+console.log(typeof isDeveloper);
+console.log(typeof x);
+console.log(typeof y);
+
